2bcfe1d1c4c2e875858f5588bd9a23808b1e0beb
David Warshawsky <david.warshawsky@sjsu.edu>
2020-05-20 21:56:53 -0700

    Worked on data collection

--- david/data.py
+++ david/data.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import sys
 sys.path.append('./data/')
+sys.path.append('./data/stock_data/day')
 import yfinance as yf
 from dictionary import *
 
@@ -10,6 +11,8 @@ from dictionary import *
 class Data():
     start = None
     stop  = None
+    stocks = None
+    data = dict()
     """
     Access stock data
     """
@@ -27,17 +30,16 @@ class Data():
         self.start = start
         self.stop = stop
 
-    def get_stocks(self):
+    def get_stocks(self, output=None):
         """
         :param self:
         :return: A dictionary with stock tickers and industries from the S&P500.
-        :rtype: dict()
         :raises Exception: If data is missing from the Wikipedia table
-        """
+        :param bool output: Returns data if true\n"""
 
         # If you already have the S&P 500 data, retrieve it locally
-        if os.path.isfile("./stocks.csv"):
-            return csv_to_dict(open("./data/stocks.csv"))
+        if os.path.isfile("./data/stocks.csv"):
+            self.stocks = csv_to_dict(open("./data/stocks.csv"))
         # Retrieve S&P500 table from Wikipedia
         else:
             table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
@@ -53,20 +55,25 @@ class Data():
 
             # Write the dictionary to a csv file for later
             dict_to_csv(open("./data/stocks.csv", "w", newline=""),dictionary)
-            # return the stocks dictionary
-            return dictionary
+            # save the stocks dictionary
+            self.stocks = dictionary
+        if output  == True:
+            return self.stocks
+     def stock_data_loaded(self,s  ):
 
-    def get_prices(self,symbol,window_days):
-        """
-        Gets data for a certain stock based on its symbol and return
-        :param str symbol: The symbol of the stock.
-        :param int window_days: The number of days to calculate percent change for Y.
-        :return: X,y
-        int:
-        """
-        #Checks that a start and stop date have been set
+    def valid_symbol(self):
         if (self.start == None or self.stop == None):
             raise ValueError("You forgot to set start and/or stop")
+        return True
+
+    def get_stock_data(self,symbol=None,output=None):
+        """
+        Gets data for a certain stock based on its symbol and return
+        :param str symbol: The symbol of the stock, can only handle 1 name.
+        :param bool output: Returns data if true\n"""
+        #Checks that a start and stop date have been set
+        # Checks that the stock is valid
+        self.valid_symbol(symbol)
         # Load & Select Data
         stock_data = yf.download(symbol, start=self.start,end=self.stop)
 
@@ -76,26 +83,49 @@ class Data():
         stock_data = stock_data.fillna(0)
         stock_data = stock_data[['Open', 'High', 'Low', 'Close','Adj Close', 'Volume']]
         stock_data.columns = ['Open', 'High', 'Low', 'Close','Adj_Close', 'Volume']
+        self.data[symbol] = stock_data
+        if output  == True:
+            return stock_data
+
+    def split_xy(self,symbol=None,window_days=None,output=None):
+        """
+        Splits stock data.
+        :param int window_days: The number of days to calculate percent change for Y.
+        :return: X,y
+        :param bool output: Returns data if true\n"""
+        self.valid_symbol(symbol)
+        if window_days is not None:
+            y = pd.DataFrame(self.data[symbol]['Adj_Close'].pct_change(periods = window_days).values,columns=[symbol]).shift(-window_days)
+        else:
+            y = pd.DataFrame(self.data[symbol]['Adj_Close'])
+
+        # The line below does not seem necessary
+        #stock_data.columns = [x+'_'+ symbol for x in list(stock_data.columns)]
+
+        self.data[symbol].drop(['Adj_Close','Close'],axis=1, inplace=True)
+        self.data[symbol].reset_index(drop=True, inplace=True)
+        y[symbol].reset_index(drop =True, inplace =True)
 
 
-        Y = pd.DataFrame(stock_data['Adj_Close'].pct_change(periods = window_days).values,columns=[symbol]).shift(-window_days)
+        X = self.data[symbol].replace(to_replace=np.NaN,value = 0)
+        y = pd.DataFrame(y).replace(to_replace=np.NaN,value = 0)
+        if output == True:
+            return X,y
 
-        stock_data.columns = [x+'_'+ symbol for x in list(stock_data.columns)]
+    def train_test_split(self, window_days: int = None, output=None):
+        X_train = None
+        X_test = None
+        y_train = None
+        y_test  = None
 
-        stock_data.drop(['Adj_Close_'+symbol,'Close_'+SYMBOL],axis=1, inplace=True)
-        Y[SYMBOL].reset_index(drop =True, inplace =True)
-        stock_data.reset_index(drop =True, inplace =True)
-
-        X = stock_data.replace(to_replace=np.NaN,value = 0)
-        y = pd.DataFrame(Y).replace(to_replace=np.NaN,value = 0)
-
-        return X, y
-
+        if output == True:
+            return X_train,X_test,y_train,y_test
+        
     def get_data_and_features(self, window_days=15, symbols=None, industry=None):
         # If SYMBOL argument is provided and no industry is provided
         x_train = None
         y_train = None
-        if (symbols != None):
+        if symbols != None:
             # It is a single stock.
             if type(symbols) == str:
                 try:
@@ -108,7 +138,7 @@ class Data():
                     try:
                         stock_data,Y = self.retrieve_data(symbol,self.start,self.stop,window_days)
                     except KeyError:
-                         stock_data,Y = self.retrieve_data(symbol,self.start,self.stop,window_days)
+                        stock_data,Y = self.retrieve_data(symbol,self.start,self.stop,window_days)
                     x_train = pd.concat((x_train,stock_data),axis=1)
                     y_train = pd.concat((y_train,Y),axis=1)
 
@@ -132,7 +162,8 @@ class Data():
 
 def main():
     data = Data()
-    data.start_stop("2010-01-01","2010-01-01")
+    data.start_stop("2010-01-01","2015-01-01")
+    print(data.get_stocks(output=True))
 
 if __name__ == '__main__':
     main()
