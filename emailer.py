3b03bc2fe03b51652b6e0e8bbe8ceba4751dac10
David Warshawsky <david.warshawsky@sjsu.edu>
2020-06-22 22:46:03 -0700

    Looked at making new TS function for LSTM and Options

--- emailer.py
+++ emailer.py
@@ -1,19 +1,26 @@
 import smtplib,ssl
 
-port = 465
-password = "Sn2pch2ttingd00vie"
+def send_email(receiver_email,subject,message):
+    port = 465
+    sender_email = "thatdoovie@gmail.com"
+    receiver_email = "davidawarshawsky@gmail.com"
+    password = "Sn2pch2ttingd00vie"
 
-# Create a secure SSL context
-context = ssl.create_default_context()
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    message = """\
+Subject: {}
+    
+{}""".format(subject,message)
+    print(message)
+    print("From: {}".format(sender_email))
+    print("To: {}".format(receiver_email))
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+        print("sent")
 
-sender_email = "thatdoovie@gmail.com"
-receiver_email = "davidawarshawsky@gmail.com"
-message = """\
-Subject: StockShock predictions!
-
-Hello David, I am David from StockShock, here are your predictions!
-"""
-
-with smtplib.SMTP_SSL("thatdoovie@gmail.com", port, context=context) as server:
-    server.login("thatdoovie@gmail.com", password)
-    server.sendmail(sender_email, receiver_email, message)
+def main():
+    send_email("davidawarshawsky@gmail.com","Stock Prediction Stuff","I hope you enjoy")
+if __name__ == '__main__':
+    main()
