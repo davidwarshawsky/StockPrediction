13757f3e9f701a5ff4aef14dc86f824893e6de90
David Warshawsky <david.warshawsky@sjsu.edu>
2020-06-24 11:49:09 -0700

    Options data puller works

--- data/Options.py
+++ data/Options.py
@@ -2,7 +2,8 @@ import yfinance as yf
 import pandas as pd
 import os
 import sys
-
+import numpy as np
+from data.TS import series_to_supervised
 sys.path.append('data/stock_data/recommendations/')
 
 
@@ -18,33 +19,34 @@ class Options():
     def switch_stock(self,symbol):
         self.symbol = symbol
         self.ticker = yf.Ticker(self.symbol)
-        self.set_filepath()
+        self._set_filepath()
         # If you already have some puts and calls read them in from CSV
         if os.path.exists(self.puts_filepath) and os.path.exists(self.calls_filepath):
-            self.retrieve_data()
+            self._retrieve_data()
         # If the options haven not been read in before
         else:
-            self.pull_data()
+            self._pull_data()
 
-    def retrieve_data(self):
+    def _retrieve_data(self):
         print("RETRIEVING DATA FROM CSV")
         self.puts = pd.read_csv(self.puts_filepath,index_col=[0,1])
         self.calls = pd.read_csv(self.calls_filepath,index_col=[0,1])
+
         self.dates = list(self.puts.index.get_level_values(0).unique())
         print(self.puts.shape, self.calls.shape)
 
-    def pull_data(self):
+    def _pull_data(self):
         print("GETTING FRESH")
-        self.set_puts_calls()
+        self._set_puts_calls()
         self.puts, self.calls = self.get_puts_calls()
         # Save data for later
         self.puts.to_csv(self.puts_filepath)
         self.calls.to_csv(self.calls_filepath)
         print(self.puts.shape, self.calls.shape)
 
-    def update(self):
+    def _update(self):
         # Get all available dates for options
-        new_dates = self.get_options_dates(self.ticker)
+        new_dates = self._get_options_dates(self.ticker)
         # If there are new dates available for options, select them.
         if self.dates != new_dates:
             # Get dates to update
@@ -52,41 +54,43 @@ class Options():
             # Add new dates to dates
             self.dates = self.dates + dates_to_update
             # Get new options and process them
-            new_chains = self.get_option_chains(self.ticker,dates_to_update)
-            new_puts   = self.get_puts(new_chains)
-            new_calls  = self.get_calls(new_chains)
+            new_chains = self._get_option_chains(self.ticker,dates_to_update)
+            new_puts   = self._get_puts(new_chains)
+            new_calls  = self._get_calls(new_chains)
+            new_puts.to_csv(self.puts_filepath,mode="a",headers=False)
+            new_calls.to_csv(self.calls_filepath,mode="a",headers=False)
             dfn_puts   = self._process_options(new_puts,new_dates)
             dfn_calls  = self._process_options(new_calls,new_dates)
             # Combine dataframes
             self.puts  = pd.concat([self.puts,dfn_puts])
             self.calls = pd.concat([self.calls,dfn_calls])
 
-    def set_filepath(self):
+    def _set_filepath(self):
         # Format the filepath
         puts_filepath = "data/stock_data/options/p{}.csv"
         calls_filepath = "data/stock_data/options/c{}.csv"
         self.puts_filepath  = puts_filepath.format(self.symbol)
         self.calls_filepath = calls_filepath.format(self.symbol)
 
-    def set_puts_calls(self):
+    def _set_puts_calls(self):
         # Getting chains
-        dates  = self.get_options_dates(self.ticker)
-        chains = self.get_option_chains(self.ticker,dates)
+        dates  = self._get_options_dates(self.ticker)
+        chains = self._get_option_chains(self.ticker,dates)
         #Setting
-        self.puts = self._process_options(self.get_puts(chains),dates)
-        self.calls  = self._process_options(self.get_calls(chains),dates)
+        self.puts = self._process_options(self._get_puts(chains),dates)
+        self.calls  = self._process_options(self._get_calls(chains),dates)
 
     def get_puts_calls(self):
         return self.puts,self.calls
 
 
-    def get_options_dates(self,ticker):
+    def _get_options_dates(self,ticker):
         """
         Gets the dates available for options for a stock.
         """
         return list(ticker.options)
 
-    def get_option_chains(self,ticker,date):
+    def _get_option_chains(self,ticker,date):
         """
         Gets option chains for a stock.
         :param date: The date or dates for the options chains.
@@ -102,7 +106,7 @@ class Options():
 
         return chains
 
-    def get_calls(self,chains):
+    def _get_calls(self,chains):
         """
         Gets calls from option chains..
         """
@@ -111,7 +115,7 @@ class Options():
             calls.append(chain.calls)
         return calls
 
-    def get_puts(self,chains):
+    def _get_puts(self,chains):
         """
         Gets puts from option chains.
         """
@@ -123,10 +127,13 @@ class Options():
     def _process_options(self,df, dates):
         df = pd.concat(df, keys=dict(zip(dates, df)))
         df['inTheMoney'] = df['inTheMoney'].replace({False: 0, True: 1})
+        df['inTheMoney'] = df['inTheMoney'].astype(np.bool)
+        df['impliedVolatility'] = df['impliedVolatility'].astype(np.int32)
         df = df.drop(columns=['lastTradeDate','contractSymbol', 'contractSize', 'currency'])
+        df = df.round(2)
         return df
 
-    def get_rec(symbol, printit=False):
+    def _get_rec(symbol, printit=False):
         ticker = yf.Ticker(symbol)
         df = ticker.recommendations
         if printit:
@@ -141,12 +148,12 @@ class Options():
 
 
 def main():
-    dates = get_options_dates("AAPL")
-    chains = get_option_chains("AAPL", dates)
-    calls = get_calls(chains)
-    puts = get_puts(chains)
-    df_puts = _process_options(puts, dates)
-    print(df_puts.head())
+    optionsHandler = Options("AMD")
+    puts,calls = optionsHandler.get_puts_calls()
+    print(puts.columns,puts.dtypes)
+    print(puts.head())
+    # df_puts = series_to_supervised(puts,10,1)
+    # print(df_puts.info(),df_puts.shape,df_puts.columns)
 
 
 if __name__ == '__main__':
