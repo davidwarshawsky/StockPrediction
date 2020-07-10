262e8fe5aade5f59a5ab6d1976a788b9111c9500
David Warshawsky <david.warshawsky@sjsu.edu>
2020-07-09 17:27:59 -0700

    Showed how prediction_tester.py functions are meant to be used

--- tests/ml_tester.py
+++ tests/ml_tester.py
@@ -1,6 +1,10 @@
 import unittest
+import sys
+sys.path.append('appdata/stock_data/predictions/')
 from ml.WaveNet import WaveNet
 from ml.prediction_maker import *
+from ml.prediction_tester import *
+from datetime import date,timedelta
 
 class MyTestCase(unittest.TestCase):
     # def test_wavenet(self):
@@ -17,6 +21,20 @@ class MyTestCase(unittest.TestCase):
             print(data.shape)
             for x in data.shape:
                 self.assertGreaterEqual(x,1)
+
+    def test_prediction_tester(self):
+        days = 10
+        pred_f_name = 'appdata/stock_data/predictions/wn&2020-08-11&10&20&5&100&1.csv'
+        df,date = read_predictions(pred_f_name)
+        # Makes sure that the test date is more than days(10) days ahead than the prediction date
+        # for purposes of this test.
+        self.assertFalse(valid_passed_days(date+timedelta(days=11),days))
+        accuracy_dict = score_predictions(df, days)
+        # Checks that score_predictions() function returns a dictionary
+        self.assertIsInstance(accuracy_dict,dict())
+        # Checks the length of the dictionary, the test case is meant for predictions of all SP500 stocks.
+        self.assertEqual(len(accuracy_dict.values()),503)
+
 
 if __name__ == '__main__':
     unittest.main()
