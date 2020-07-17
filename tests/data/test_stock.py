56354b613d16d8416f7d258269a7b738835cded6
David Warshawsky <david.warshawsky@sjsu.edu>
2020-07-16 20:44:41 -0700

    added stock tests

--- tests/data/test_stock.py
+++ tests/data/test_stock.py
@@ -1,9 +1,10 @@
 from src.data.Stock import Stock
-
+from datetime import datetime
+import pytest
 class TestInit():
-    def test_no_args(self):
+    def test_none(self):
         stockHandler = Stock()
-        actual_start_date = stockHandler.start
+        actual_start_date = stockHandler.start.strftime("%Y-%m-%d")
         default_start_date = '2010-01-01'
         start_message = 'Expected default start date: {}' \
                   'Actual start date: {}'
@@ -11,10 +12,33 @@ class TestInit():
         symbol_mesage = "Symbol should be None but is {} ".format(stockHandler.symbol)
         assert stockHandler.symbol == None, symbol_mesage
 
-    def test_symbol_arg(self):
+    def test_symbol(self):
         symbol = "GOOG"
         stockHandler = Stock(symbol)
+        actual_start_date = stockHandler.start.strftime("%Y-%m-%d")
         default_start_date = '2010-01-01'
         start_message = 'Expected default start date: {}' \
                         'Actual start date: {}'
+        assert actual_start_date == default_start_date, start_message
+        symbol_mesage = "Expected symbol {} Actual symbol {}".format(symbol,stockHandler.symbol)
+        assert stockHandler.symbol == symbol, symbol_mesage
+
+    @pytest.mark.xfail
+    def test_bad_symbol(self):
+        symbol = "BADARGUMENT"
+        with pytest.raises(ValueError) as exception_info:
+            stockHandler = Stock(symbol)
+        expected_message = "{} is not a valid yfinance symbol".format(symbol)
+        message = "{} expected error message not received".format(symbol)
+        assert exception_info == expected_message,message
+
+    def test_bad_date(self):
+        symbol = "AMD"
+        date = "20201-0-01"
+        with pytest.raises(ValueError) as exception_info:
+            stockHandler = Stock(symbol,date)
+
+
+        
+
         
