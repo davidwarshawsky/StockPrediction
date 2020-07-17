a6e0c5f94cf6125554116ea7a21e34a57d289377
davidwarshawsky <davidawarshawsky@gmail.com>
2020-07-16 18:09:48 -0700

    Started working on unit testing the Stock class

--- tests/data/test_stock.py
+++ tests/data/test_stock.py
@@ -1,4 +1,20 @@
 from src.data.Stock import Stock
 
 class TestInit():
-    def
+    def test_no_args(self):
+        stockHandler = Stock()
+        actual_start_date = stockHandler.start
+        default_start_date = '2010-01-01'
+        start_message = 'Expected default start date: {}' \
+                  'Actual start date: {}'
+        assert  actual_start_date == default_start_date,start_message
+        symbol_mesage = "Symbol should be None but is {} ".format(stockHandler.symbol)
+        assert stockHandler.symbol == None, symbol_mesage
+
+    def test_symbol_arg(self):
+        symbol = "GOOG"
+        stockHandler = Stock(symbol)
+        default_start_date = '2010-01-01'
+        start_message = 'Expected default start date: {}' \
+                        'Actual start date: {}'
+        
