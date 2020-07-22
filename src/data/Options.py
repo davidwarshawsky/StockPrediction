472f1abd6b01cee75e421e8017296f47a5d74e11
David Warshawsky <david.warshawsky@sjsu.edu>
2020-07-22 09:44:18 -0700

    Made Options class similar to Stock class

--- src/data/Options.py
+++ src/data/Options.py
@@ -4,25 +4,29 @@ import os
 import sys
 import numpy as np
 from datetime import datetime
-sys.path.append('appdata/stock_data/recommendations/')
-from src.features.TSDS import TSDS
+sys.path.append('appdata{0}stock_data{0}recommendations{0}')
 from src.features.TS import validate
+from src.features.TSDS import TSDS
 
 # https://github.com/mcdallas/wallstreet
 
-class Option(TSDS):
+class Options(TSDS):
     """
     A stock data structure to hold stock data
     """
     _puts: pd.DataFrame
     _calls: pd.DataFrame
-    _symbol: str
+    _symbol: str = None
     _ticker: yf.Ticker
     _puts_path  = None
     _calls_path = None
     _dates      = None
 
     def __init__(self,symbol:str=None,start:str='2010-01-01'):
+        """
+
+        :rtype: object
+        """
         # Set the start regardless of if there is a symbol provided.
         self.switch_start(start)
         # If the symbol is provided, switch to it.
@@ -52,6 +56,7 @@ class Option(TSDS):
         # Closer to present is larger
         else:
             self._start = new_start
+            return True
 
     def __set_symbol(self,symbol):
         # Check if the symbol has data available.
@@ -62,57 +67,42 @@ class Option(TSDS):
             raise ValueError(message)
         # If the symbol has available data, set it as the current symbol.
         self._symbol = symbol
+        self._ticker = yf.Ticker(self._symbol)
 
     def __set_path(self):
         # Format the filepath
-        puts_path = "appdata/stock_data/options/p{}.csv"
-        calls_path = "appdata/stock_data/options/c{}.csv"
-        self._puts_path = puts_path.format(self._symbol)
-        self._calls_path = calls_path.format(self._symbol)
+        calls_path = "appdata{0}stock_data{0}options{0}c{1}.csv"
+        puts_path = "data{0}stock_data{0}options{0}p{1}.csv"
+        self._calls_path = calls_path.format(os.path.sep,self._symbol)
+        self._puts_path = puts_path.format(os.path.sep,self._symbol)
 
     def __retrieve_data(self):
         """
         Retrieves puts and calls dataframes from csv.
         :return:
         """
-
-        def __retrieve_data(self):
-            # check if data is already available
-            if os.path.isfile(self._calls_path) and os.path.isfile(self._puts_path):
-                # Read in all data and select a subset based on the start in case last time
-                # the Stock was called an earlier start date was used.
-                self._puts = pd.read_csv(self._puts_path, index_col=[0, 1])
-                self._calls = pd.read_csv(self._calls_path, index_col=[0, 1])
-                self._dates = list(self._puts.index.get_level_values(0).unique())
-                return "r"
-            else:
-                # Load & Select ALL Data, you don't get a choice.
-                # You get to choose a subsection of ALL THE DATA.
-                data = yf.download(self._symbol, rounding=True)
-                self._all_data = self.__process_data(data)
-                # Save the data for later.
-                self.__save_data(self._all_data, append=False)
-                # Return
-                return "w"
-
-    def __pull_data(self):
-        print("GETTING FRESH")
-        self._set_puts_calls()
-        self.puts, self.calls = self.__get_calls_puts()
-        # Save data for later
-        self._puts.to_csv(self.puts_filepath)
-        self._calls.to_csv(self.calls_filepath)
-
-    def __set_puts_calls(self):
-        # Getting chains
-        dates  = self.__get_options_dates(self.ticker)
-        chains = self._get_option_chains(self.ticker,dates)
-        #Setting
-        self._puts = self._process_data(self._get_puts(chains),dates)
-        self._calls  = self._process_data(self._get_calls(chains),dates)
+        # check if data is already available
+        if os.path.isfile(self._calls_path) and os.path.isfile(self._puts_path):
+            self._calls = pd.read_csv(self._calls_path, index_col=[0, 1])
+            self._puts = pd.read_csv(self._puts_path, index_col=[0, 1])
+            self._dates = list(self._puts.index.get_level_values(0).unique())
+            return "r"
+        else:
+            print("GETTING FRESH")
+            # Getting chains
+            dates = self.__get_options_dates(self._ticker)
+            chains = self.__get_options_chains(self._ticker, dates)
+            # Setting
+            self._puts = self.__process_data(self.__get_puts(chains), dates)
+            self._calls = self.__process_data(self.__get_calls(chains), dates)
+            self._set_puts_calls()
+            self._calls, self._puts = self.__get_calls_puts()
+            # Save data for later
+            self._puts.to_csv(self._puts_path)
+            self._calls.to_csv(self._calls_path)
 
     def __get_calls_puts(self):
-        return self.puts,self.calls
+        return self._calls,self._puts
 
 
     def __get_options_dates(self,ticker):
@@ -121,7 +111,7 @@ class Option(TSDS):
         """
         return list(ticker.options)
 
-    def __get_option_chains(self,ticker,date):
+    def __get_options_chains(self,ticker,date):
         """
         Gets option chains for a stock.
         :param date: The date or dates for the options chains.
@@ -164,29 +154,38 @@ class Option(TSDS):
         df = df.round(2)
         return df
 
+    def __save_data(self, df, path,append=False):
+        if not append:
+            df.to_csv(path, index=True)
+        else:
+            df.to_csv(path, mode="a", index=True, header=False)
 
     def __update_data(self):
-        # Get all available dates for options
-        new_dates = self.__get_options_dates(self.ticker)
+        # Get all current available dates for options
+        options_dates = self.__get_options_dates(self._ticker)
         # If there are new dates available for options, select them.
-        if self._dates != new_dates:
+        if not all(x in self._dates for x in options_dates):
             # Get dates to update
-            new_dates = [x for x in new_dates if x not in self.dates]
+            new_dates = [x for x in options_dates if x not in self._dates]
             # Add new dates to dates
-            self.dates = self._dates.append(new_dates)
+            self._dates = self._dates.append(new_dates)
             # Get new options and process them
-            new_chains = self._get_option_chains(self.ticker, dates_to_update)
-            new_puts = self._get_puts(new_chains)
-            new_calls = self._get_calls(new_chains)
-            new_puts.to_csv(self.puts_filepath, mode="a", headers=False)
-            new_calls.to_csv(self.calls_filepath, mode="a", headers=False)
-            dfn_puts = self.__process_data(new_puts, new_dates)
-            dfn_calls = self.__process_data(new_calls, new_dates)
-            # Combine dataframes
-            self.puts = pd.concat([self.puts, dfn_puts])
-            self.calls = pd.concat([self.calls, dfn_calls])
+            new_chains = self._get_options_chains(self._ticker, new_dates)
 
-    def _get_rec(symbol, printit=False):
+            new_calls = self.__get_calls(new_chains)
+            new_processed_calls = self.__process_data(new_calls, new_dates)
+            new_puts = self.__get_puts(new_chains)
+            new_processed_puts = self.__process_data(new_puts, new_dates)
+
+            # Save new data
+            self.__save_data(new_processed_puts,self._puts_path,append=True)
+            self.__save_data(new_processed_calls,self._calls_path,append=True)
+
+            # Combine dataframes
+            self.puts = pd.concat([self.puts, new_processed_puts])
+            self.calls = pd.concat([self.calls, new_processed_calls])
+
+    def _get_rec(self,symbol, printit=False):
         ticker = yf.Ticker(symbol)
         df = ticker.recommendations
         if printit:
