e50e50d223627a3f6f2a01b058f5d0b7222e25ee
David Warshawsky <david.warshawsky@sjsu.edu>
2020-07-28 15:45:15 -0700

    Worked on testing MakePredictorSP500 class

---
+++ tests/models/test_prediction_maker.py
@@ -0,0 +1,29 @@
+import pytest
+from src.models.prediction_maker import ModelPredictorSP500
+class TestReadInvalidSymbols(object):
+    def test_len_symbols(self):
+        expected_length = 2
+        invalid_symbols = ModelPredictorSP500.read_invalid_symbols()
+        actual_length = len(invalid_symbols)
+        message = "Expected length: <{}> Actual length <{}>".format(expected_length,actual_length)
+        assert actual_length == expected_length,message
+
+    def test_type(self):
+        expected_type = list
+        actual_type   = type(ModelPredictorSP500.read_invalid_symbols())
+        message = "Expected type <{}> Actual type <{}>".format(expected_type,actual_type)
+        assert actual_type == expected_type,message
+
+class TestReadSP500Symbols(object):
+    def test_len_symbols(self):
+        expected_length = 503
+        symbols = ModelPredictorSP500.read_SP500_symbols()
+        actual_length = len(symbols)
+        message = "Expected length: <{}> Actual length <{}>".format(expected_length, actual_length)
+        assert actual_length == expected_length, message
+
+    def test_type(self):
+        expected_type = list
+        actual_type = type(ModelPredictorSP500.read_SP500_symbols())
+        message = "Expected type <{}> Actual type <{}>".format(expected_type, actual_type)
+        assert actual_type == expected_type, message
