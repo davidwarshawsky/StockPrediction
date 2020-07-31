171dfd785ddc0b88dbf89a245d9ba030828544df
David Warshawsky <david.warshawsky@sjsu.edu>
2020-07-31 11:52:58 -0700

    Moved over Stock.ipynb functions

---
+++ src/features/Feature_Generator.py
@@ -0,0 +1,38 @@
+from sklearn.metrics import mean_absolute_error,accuracy_score
+import numpy as np
+
+class FeatureGenerator():
+    @staticmethod
+    def GAP_LAP(X,windows=[1,2,5,10]):
+        """
+        Calculates gap lap.
+        Taken from Stock.ipynb
+        :param X:
+        :param windows:
+        :return:
+        """
+        for window in windows:
+            X['GapUp_'+str(window)] = (X['Open'].shift(-window).fillna(value = 0) > X['High']).astype(int)
+            X['LapUp_'+str(window)] = (X['Open'].shift(-window).fillna(value = 0) > X['Adj_Close']).astype(int)
+            X['GapUp_Sum_'+str(window)] = (X['GapUp_'+str(window)].rolling(5).aggregate(lambda x : x.sum()))
+            X['LapUp_Sum_'+str(window)] = (X['LapUp_'+str(window)].rolling(5).aggregate(lambda x : x.sum()))
+        return X
+
+    @staticmethod
+    def comparitive_stds(X, windows=[10]):
+        """
+        Calculates comparitive standard deviations.
+        Taken from Stock.ipynb
+        :param windows:
+        :return:
+        """
+        for window in windows:
+            base = X['Open', 'High', 'Low'].mean(axis=0)
+            local_means = base.rolling(window).mean()[::window].tolist()
+            for mean in local_means:
+                local_groups = np.array_split(base, window)
+    @staticmethod
+    def categorical(pred, real):
+        pred = np.where(pred > 0, 1, 0)
+        real = np.where(real > 0, 1, 0)
+        return accuracy_score(real, pred)
